624361194f5e1dac4cb6d102e853f9a76cae134c
YvesDeutschmann <yves.deutschmann@gmail.com>
2020-05-29 11:25:47 +0200

    passed data from backend to frontend

--- worldbankapp/routes.py
+++ worldbankapp/routes.py
@@ -4,12 +4,10 @@ from wrangling_scripts.wrangling import data_wrangling
 
 data = data_wrangling()
 
-print(data)
-
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data_set=data)
 
 @app.route('/project-one')
 def project_one():
